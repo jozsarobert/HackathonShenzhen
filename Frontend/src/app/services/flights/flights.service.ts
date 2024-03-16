565ac3c537caf7159f48ed577e62396f87d81a73
Nikolai Weidner <niko.weidner@gmx.de>
2024-03-17 00:11:26 +0800

    [Frontend] Connect Search Page to Mock search using FlightDto

--- Frontend/src/app/services/flights/flights.service.ts
+++ Frontend/src/app/services/flights/flights.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject, of } from 'rxjs';
+import { FlightDto } from '../../../model/flightDto';
 
 @Injectable({
   providedIn: 'root',
@@ -31,6 +32,31 @@ export class FlightsService {
         { title: 'Destination', value: 'JFK' },
         { title: 'Shipments', value: '2' },
         { title: 'Status', value: 'Alert' },
+      ],
+    });
+  }
+
+  // todo replce mock with real backend
+  public mockGetFlightByFlightnumberAndDate(): Observable<FlightDto> {
+    return of({
+      flightNo: 'LX14',
+      originCode: 'ZRH',
+      departureDateTime: '16.03.2024',
+      destinationlCode: 'JFK',
+      arrivalDateTime: '2023-01-01T06:00', // todo
+      shipments: [
+        {
+          id: 'todo',
+          waybillNumber: '724-14329302',
+          pieces: ['piece1', 'piece2'],
+          hasAlert: false,
+        },
+        {
+          id: 'todo',
+          waybillNumber: '724-14329302',
+          pieces: ['piece1', 'piece2'],
+          hasAlert: true,
+        },
       ],
     });
   }
--- Frontend/src/app/views/search/search/search.component.ts
+++ Frontend/src/app/views/search/search/search.component.ts
@@ -1,18 +1,113 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { cilCalendar } from '@coreui/icons';
+import { Subscription } from 'rxjs';
+import { FlightsService } from 'src/app/services/flights/flights.service';
+import { FlightDto } from 'src/model/flightDto';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   icons = { cilCalendar };
 
-  public constructor(private route: ActivatedRoute, private router: Router) {}
+  searchForm: FormGroup = new FormGroup({
+    shipmentNo: new FormControl(null),
+    flight: new FormControl(null),
+    date: new FormControl(null),
+    origin: new FormControl(null),
+    destination: new FormControl(null),
+    pieceId: new FormControl(null),
+  });
+
+  isAnyFieldTouched: boolean = false;
+
+  subscriptions: Subscription[] = [];
+
+  public flight: FlightDto | undefined = undefined;
+  public flightDisplayValues: any;
+  public flightHasAlert: boolean = false; // loop through shipments and check if any has alert
+
+  public constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private flightsService: FlightsService
+  ) {}
+
+  public ngOnInit(): void {
+    //this.subscribeToStatusChanges();
+  }
+
+  // disable every field except the one that is being filled
+  // private subscribeToStatusChanges(): void {
+  //   Object.keys(this.searchForm.controls).forEach((key) => {
+  //     const subscription = this.searchForm
+  //       .get(key)
+  //       ?.statusChanges.subscribe((status) => {
+  //         if (status === 'VALID' && this.searchForm?.get(key)?.value) {
+  //           this.unsubscribeFromStatusChanges();
+  //           Object.keys(this.searchForm.controls).forEach((controlKey) => {
+  //             if (controlKey !== key) {
+  //               this.searchForm?.get(controlKey)?.disable();
+  //             }
+  //           });
+  //           this.subscribeToStatusChanges();
+  //         } else if (status === 'VALID' && !this.searchForm.get(key)?.value) {
+  //           this.unsubscribeFromStatusChanges();
+  //           Object.keys(this.searchForm.controls).forEach((controlKey) => {
+  //             this.searchForm.get(controlKey)?.enable();
+  //           });
+  //           this.subscribeToStatusChanges();
+  //         }
+  //       });
+  //     if (subscription) {
+  //       this.subscriptions.push(subscription);
+  //     }
+  //   });
+  // }
 
   public navigateToPage(page: string) {
     this.router.navigate([page]);
   }
+
+  // todo rename
+  public getFilledOutFieldIds(): void {
+    // let filledOutFieldIds: string[] = [];
+    // Object.keys(this.searchForm.controls).forEach((controlKey) => {
+    //   if (this.searchForm.get(controlKey)?.value) {
+    //     filledOutFieldIds.push(controlKey);
+    //   }
+    // });
+
+    // todo get type selected and fetch data according to type
+    this.flightsService
+      .mockGetFlightByFlightnumberAndDate()
+      .subscribe((flight: FlightDto) => {
+        this.flight = flight;
+        this.flightDisplayValues = {
+          id: flight.flightNo,
+          values: [
+            { title: 'Date', value: flight.departureDateTime },
+            { title: 'Origin', value: flight.originCode },
+            { title: 'Destination', value: flight.destinationlCode },
+            { title: 'Shipments', value: flight.shipments?.length },
+          ],
+        };
+
+        this.flightHasAlert =
+          flight.shipments?.some((shipment) => shipment.hasAlert) || false;
+      });
+  }
+
+  public navigateToFlightDetail(flightId: string): void {
+    this.router.navigate([`flights/detail/${flightId}`]);
+  }
+
+  // private unsubscribeFromStatusChanges(): void {
+  //   this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  //   this.subscriptions = [];
+  // }
 }
