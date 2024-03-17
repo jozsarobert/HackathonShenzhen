114ef26ce8005ba7f00135dbd6c708f89b770005
Nikolai Weidner <niko.weidner@gmx.de>
2024-03-17 11:19:44 +0800

    [Frontend] Fix title

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'LEX - todo';
+  title = 'LEX - Location and Environmental Exchange';
 
   constructor(
     private router: Router,
